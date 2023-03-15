cae727018fe1df76e5b1bbfdd1eb664038a4f380
NKking23 <127386774+NKking23@users.noreply.github.com>
2023-03-15 17:10:52 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,14 +15,6 @@ pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
 pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
 pins.touchSetMode(TouchTarget.LOGO, TouchTargetMode.Capacitive)
 basic.forever(function () {
-    while (input.buttonIsPressed(Button.A)) {
-        media.sendCode(media.keys(media._MediaKey.vol_down))
-    }
-    while (input.buttonIsPressed(Button.B)) {
-        media.sendCode(media.keys(media._MediaKey.vol_up))
-    }
-})
-basic.forever(function () {
     while (input.pinIsPressed(TouchPin.P2)) {
         if (50 > input.acceleration(Dimension.X) || -50 < input.acceleration(Dimension.X)) {
             mouse.movexy(input.acceleration(Dimension.X) / 20, 0)
@@ -30,5 +22,13 @@ basic.forever(function () {
         if (50 > input.acceleration(Dimension.Y) || -50 < input.acceleration(Dimension.Y)) {
             mouse.movexy(0, input.acceleration(Dimension.Y) / 20)
         }
+    }
+})
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.A)) {
+        media.sendCode(media.keys(media._MediaKey.vol_down))
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        media.sendCode(media.keys(media._MediaKey.vol_up))
     }
 })
